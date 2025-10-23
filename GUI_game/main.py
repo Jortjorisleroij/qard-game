9e81a7a0bfff9808a7d2c8dd8417b71289680b51
Jort Leroij <111754184+Jortjorisleroij@users.noreply.github.com>
2025-10-23 10:42:48 +0200

    Refactor imports and update card handling

--- GUI_game/main.py
+++ GUI_game/main.py
@@ -1,21 +1,13 @@
-# -*- coding: utf-8 -*-
 """
 Main entry point for GUI application.
 """
 
-from GUI_game import (
-    GUI,
-    ImageLoader,
-    Display_full_deck,
-    Display_first_card,
-    Build_buttons,
-    Display_player_decks,
-    Display_player_cards
-)
+from GUI_game import (GUI,ImageLoader,Display_full_deck,Display_first_card,
+                      Build_buttons,Display_player_decks,Display_player_cards)
 
 
-example_cards = ["C_3", "T_5", "R_1", "super", "T_cr", "C_sp", "T_1", "T_2", "T_3", "T_4", "T_5"]
-
+cards = ["C_3", "T_5", "R_1", "super", "T_cr", "C_sp", "T_1", "T_2", "T_3", "T_4", "T_5"]
+clickables = [1,0,0,0,1,1,0,0,0,0,0]
 
 def main():
     # Initialize GUI
@@ -30,10 +22,9 @@ def main():
     buttons.manage_buttons()
 
 
-
     # the part that interacts with the backend.
     Display_player_decks(root, image_loader, offset_x=0.3, width=300, height=200, P1=8, P2=6)
-    Display_player_cards(root, image_loader, example_cards, width=150, height=200)
+    player_cards = Display_player_cards(root, image_loader, cards, clickables)
 
     # Start the main event loop
     root.mainloop()
