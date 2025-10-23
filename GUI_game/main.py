1cc6c753a70b97d7ae11cf1573d910f132933ce1
Jort Leroij <111754184+Jortjorisleroij@users.noreply.github.com>
2025-10-23 12:47:48 +0200

    Update main.py

--- GUI_game/main.py
+++ GUI_game/main.py
@@ -1,49 +1,42 @@
-"""
-Main entry point for GUI application.
-"""
+from GUI_game import (GUI, ImageLoader, Display_full_deck, Display_first_card,
+                      Build_buttons, Display_player_decks, Display_player_cards)
 
-from GUI_game import (GUI,ImageLoader,Display_full_deck,Display_first_card,
-                      Build_buttons,Display_player_decks,Display_player_cards)
-
-
-
-# this is an example card pack. the GUI should receive this from a function in the back-end
+# Example card pack
 cards = ["C_3", "T_5", "R_1", "super", "T_cr", "C_sp", "T_1", "T_2", "T_3", "T_4", "T_5"]
-clickables = [1,0,0,0,1,1,0,0,0,0,0]
+clickables = [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0]
 
 
-
-def main():
-    #############################################
-    # Initialisation of the interface of the game.
-    #############################################
+def Initialize_start_game():
+    # Initialize GUI
     app = GUI()
+    app.ask_player_names()
     root = app.main_window()
     app.draw_black_square()
     app.draw_table_square()
-    image_loader = ImageLoader(bg_color=(0, 128, 0))
-    app.ask_player_names()
     app.display_player_names()
-    Display_full_deck(root, image_loader, width=200, height=200)
+    image_loader = ImageLoader(bg_color=(0, 128, 0))
+    Display_full_deck(root, image_loader)
     Display_first_card(root, image_loader, random=True)
     buttons = Build_buttons(root)
     buttons.manage_buttons()
     
+    return app, root, image_loader
+
+
+
+# This part of the main should probably be completely handled by another script. so the back-end. 
+def update_game():
+    app, root, image_loader = Initialize_start_game()
     
-    ################################################
-    # commands to display things throughout the game
-    ################################################
-    Display_player_decks(root, image_loader, offset_x=0.3, width=300, height=200, P1=8, P2=6)
+    # Display player decks and cards
+    Display_player_decks(root, image_loader, P1=8, P2=6)
     player_cards = Display_player_cards(root, image_loader, cards, clickables)
-    Display_first_card(root, image_loader, card_name="C_1", random=False)
     
-    # command to rotate names.
-    #root.after(1000, lambda: app.rotate_player_names())
+    # Example: rotate names after 1 second
+    # root.after(1000, lambda: app.rotate_player_names())
     
-    
-    # Start the main event loop
     root.mainloop()
 
 
 if __name__ == "__main__":
-    main()
+    update_game()
